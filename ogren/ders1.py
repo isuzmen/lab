46a42838dcefd1c6b452e29dbb0289e4c23f8139
İlyas <https://github.com/isuzmen/lab.git>
2026-09-21 13:11:43 +0300

    çalışma yapıldı

---
+++ ogren/ders1.py
@@ -0,0 +1,9 @@
+sehir = "Ankara"
+sicaklik = "23"
+oran = 4.5
+print(sehir)
+print(sicaklik)
+print(oran)
+print(type(sehir))
+print(type(sicaklik))
+print(type(oran))
---
+++ ogren/ders1b.py
@@ -0,0 +1,4 @@
+print(23+1)
+print("23"+"1")
+print("23"+ str(1))
+print(int("23") + 1)
